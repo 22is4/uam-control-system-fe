d2b99f36404c735ec9a07b0b49b384c512118818
Village_Water <yeon030201@gmail.com>
2024-10-03 14:47:49 +0900

    feat: default view setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
-import { Viewer } from 'resium';
+import { Ion, JulianDate } from 'cesium';
+import { Clock, Viewer } from 'resium';
+import styled from 'styled-components';
+
+const UCSViewer = styled(Viewer)`
+  width: 100%;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`;
 
 function App() {
-  return <Viewer full />;
+  Ion.defaultAccessToken = import.meta.env.VITE_CESIUM_ACCESSTOKEN;
+
+  return (
+    <UCSViewer
+      baseLayerPicker={false}
+      fullscreenButton={false}
+      geocoder={false}
+      homeButton={false}
+      infoBox={false}
+      sceneModePicker={false}
+      selectionIndicator={false}
+      timeline={false}
+      navigationHelpButton={false}
+      navigationInstructionsInitiallyVisible={false}
+      //creditContainer={document.createElement('div')} //
+      skyBox={false}
+    >
+      <Clock startTime={JulianDate.now()} shouldAnimate={true} />
+    </UCSViewer>
+  );
 }
 
 export default App;
