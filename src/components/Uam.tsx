20fc5b8e5fdbd642d84b3ff95d5f1d11d5982ada
Village_Water <yeon030201@gmail.com>
2024-10-12 13:12:06 +0900

    fix: Uam Entity의 availability attribute 제거

--- src/components/Uam.tsx
+++ src/components/Uam.tsx
@@ -1,12 +1,5 @@
-import {
-  Cartesian3,
-  Color,
-  JulianDate,
-  SampledPositionProperty,
-  TimeInterval,
-  TimeIntervalCollection,
-} from 'cesium';
-import { Entity, PathGraphics } from 'resium';
+import { Cartesian3, Color, JulianDate, SampledPositionProperty } from 'cesium';
+import { Entity } from 'resium';
 
 type UamProps = {
   uamTime: JulianDate[];
@@ -20,22 +13,12 @@ export default function Uam({ uamTime, uamCooordinates }: UamProps) {
   return (
     <>
       <Entity
-        availability={
-          new TimeIntervalCollection([
-            new TimeInterval({
-              start: uamTime[0],
-              stop: uamTime[uamTime.length - 1],
-            }),
-          ])
-        }
         point={{
           pixelSize: 10,
           color: Color.fromRandom({ minimumAlpha: 1 }),
         }}
         position={positionProperty}
-      >
-        <PathGraphics width={1} />
-      </Entity>
+      ></Entity>
     </>
   );
 }
