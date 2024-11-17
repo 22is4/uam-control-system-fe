c8935d6d349a62e33e573365b771d3ef7d5cbad0
Village_Water <yeon030201@gmail.com>
2024-11-17 21:00:41 +0900

    Revert "Merge branch 'develop' into develop"
    This reverts commit e341801c25b977f9e22b6ef0c5804db3f946da81, reversing
    changes made to 053f65bc562c36e4236dd7a3a2a6a3b711aa0c78.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-
 import { useUamInstanceStore } from './uam/uamInstance';
 import UCSViewer from './viewer/UCSViewer';
 import styled from 'styled-components';
 import { ViewerProvider } from './viewer/ViewerContext';
 import UCSInfoPanel from './viewer/UCSInfopanel';
-
 
 const AppContainer = styled.div`
   display: flex;
