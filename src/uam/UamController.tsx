46b7cd9541bd318e1e2e621acdef412c573494aa
Village_Water <yeon030201@gmail.com>
2024-11-21 23:23:15 +0900

    feat: Uam Controller api 요청 추가

--- src/uam/UamController.tsx
+++ src/uam/UamController.tsx
@@ -2,14 +2,33 @@ import { useEffect } from 'react';
 import Uam from './UamEntity';
 import { useUamInstanceStore } from './uamInstance';
 import { UNIT_TIME } from './constants';
+import axios from 'axios';
 
 const UamController: React.FC = () => {
   const { uamInstances, setUamInstance } = useUamInstanceStore();
 
+  const getUamInstances = async () => {
+    try {
+      // 서버에 데이터 요청
+      const response = await axios.get(
+        `http://cocoquiet.com:10001/uam/drone/instances`,
+      );
+
+      const res = response.data;
+      const instances = [];
+      for (let index = 0; index < res.length; index++) {
+        instances.push(res[index]['instanceId']);
+      }
+
+      setUamInstance(instances);
+    } catch (error) {
+      console.error(`Failed to fetch uam Instances`, error);
+    }
+  };
+
   useEffect(() => {
-    //TODO: api code로 변경
     const intervalId = setInterval(() => {
-      setUamInstance([0, 1, 2, 4]);
+      getUamInstances();
     }, 1000 * UNIT_TIME);
 
     return () => clearInterval(intervalId);
