f772898619ab6bb566ebe19319888c97c18add24
Village_Water <yeon030201@gmail.com>
2024-11-25 00:43:04 +0900

    feat: add wasm file

---
+++ src/wasm/uam_control_system_wasm_bg.wasm.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_uam_free(a: number, b: number): void;
+export function __wbg_uams_free(a: number, b: number): void;
+export function uam_new(a: number, b: number, c: number, d: number, e: number, f: number): number;
+export function uam_predict_collision(a: number, b: number, c: number): void;
+export function uams_new(): number;
+export function uams_add(a: number, b: number): void;
+export function uams_predict_collisions(a: number): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
